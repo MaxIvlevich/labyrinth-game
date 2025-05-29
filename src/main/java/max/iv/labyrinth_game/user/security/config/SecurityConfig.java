5cd8c1f0f19289f2814f938f612b44f690d3df4f
MaxIvlev <maxivichmax@gmail.com>
2025-05-29 15:01:19 +0300

    feat: add jwt token generation

--- src/main/java/max/iv/labyrinth_game/user/security/config/SecurityConfig.java
+++ src/main/java/max/iv/labyrinth_game/user/security/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package max.iv.labyrinth_game.user.security.config;
 
+import max.iv.labyrinth_game.user.security.jwt.AuthTokenFilter;
+import max.iv.labyrinth_game.user.security.jwt.JwtAuthEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,11 +15,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final JwtAuthEntryPoint unauthorizedHandler;
+    private final AuthTokenFilter authenticationJwtTokenFilter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthEntryPoint unauthorizedHandler,
+                          AuthTokenFilter authenticationJwtTokenFilter) {
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.authenticationJwtTokenFilter = authenticationJwtTokenFilter;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -31,6 +46,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // Разрешаем доступ без аутентификации к следующим эндпоинтам:
@@ -38,9 +54,10 @@ public class SecurityConfig {
                         .requestMatchers("/game/**").permitAll()
                         .anyRequest().authenticated()
                 );
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
 
-        // TODO:  JWT фильтр:
-        // http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(authenticationJwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
